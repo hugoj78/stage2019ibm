28ac19458cb5b6110016bd6e3245a4cfc95b6ec9
Hugo <hugoj.egu@gmail.com>
2019-09-01 20:06:53 +0200

    social media

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,7 +11,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "hugojeanegu@gmail.com"; 
+$to = "hugojean.egu@ynov.com"; 
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.

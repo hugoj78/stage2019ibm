f5551c2bfc006bad51b616c9776fc90c78adbb22
Hugo <hugoj.egu@gmail.com>
2019-09-02 00:22:09 +0200

    form

---
+++ mail/contact_me.php
@@ -0,0 +1,10 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+else {
+  http_response_code(200);
+}
+?>
